d73dcaec7c60f5e6d9a25cb53a7232e3636c41ee
BuffDave <arinodavejoshua@gmail.com>
2025-01-13 11:39:44 +0800

    Initial commit

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,10 +4,17 @@ module.exports = {
   plugins: [
     purgecss({
       content: [
-        './*.html',              // Scan HTML files in the root directory
-        './src/**/*.{js,scss}',  // Scan JS and SCSS files in the `src` folder
-        './public/**/*.svg',     // Optional: Include public assets like SVGs if needed
+        './*.html',
+        './src/**/*.{js,scss}',
+        './public/**/*.svg', 
       ],
+      safelist: {
+        standard: [
+          /^collapse/,
+          /^navbar/,
+          /^show/,
+        ],
+      },
     }),
   ],
 };
